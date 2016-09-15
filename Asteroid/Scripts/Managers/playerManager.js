1f373f210c53beeaa84bb0f3205d88d8ee29a068
Zeno Saviourmarsalin <u1077160@ad.utah.edu>
2016-09-15 17:02:27 -0600

    Added player sprite rendering

---
+++ Asteroid/Scripts/Managers/playerManager.js
@@ -0,0 +1,17 @@
+﻿
+function playerManagerLoad(thisGame) {
+    thisGame.load.image('ship', 'Assets/Images/ship.png');
+}
+
+function playerManagerCreate(thisGame) {
+    createPlayer(thisGame);
+}
+
+function playerManagerUpdate() {
+
+}
+
+function createPlayer(thisGame) {
+    this.ship = thisGame.add.sprite(10, 10, 'ship');
+    this.ship.scale.setTo(0.1);
+}
