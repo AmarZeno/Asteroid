2e57307d5523b4deee55162c4a9f380fc2e48751
Zeno Saviourmarsalin <u1077160@ad.utah.edu>
2016-09-15 17:50:52 -0600

    Added ship movements

--- Asteroid/Scripts/Managers/playerManager.js
+++ Asteroid/Scripts/Managers/playerManager.js
@@ -1,5 +1,8 @@
 ﻿var arrowKeys;
 
+/*
+ENGINE CALLS
+*/
 // Extended system preload method
 function playerManagerLoad(thisGame) {
     thisGame.load.image('ship', 'Assets/Images/ship.png');
@@ -13,22 +16,57 @@ function playerManagerCreate(thisGame) {
 
 // Extended system update method
 function playerManagerUpdate(thisGame) {
-    if (arrowKeys.left.isDown) {
-        this.ship.rotation += 0.04;
-    } else if (arrowKeys.right.isDown) {
-        this.ship.rotation -= 0.04;
-    }
+    capturePlayerActions();
 }
 
 
-// Custom Accessors
+/*
+CUSTOM ACCESSORS
+*/
 
 function addPlayer(thisGame) {
     this.ship = thisGame.add.sprite(this.game.world.centerX, this.game.world.centerY, 'ship');
     this.ship.scale.setTo(0.1);
     this.ship.anchor.setTo(0.5);
+    game.physics.enable(this.ship, Phaser.Physics.ARCADE);
 }
 
 function addPlayerControls(thisGame) {
     arrowKeys = thisGame.game.input.keyboard.createCursorKeys();
+}
+
+function capturePlayerActions() {
+    if (arrowKeys.up.isDown) {
+        game.physics.arcade.accelerationFromRotation(this.ship.rotation, 200, this.ship.body.acceleration);
+    }
+    else {
+        this.ship.body.acceleration.set(0);
+    }
+
+    if (arrowKeys.left.isDown) {
+        //this.ship.rotation -= 0.07;
+        this.ship.body.angularVelocity = -300;
+    } else if (arrowKeys.right.isDown) {
+        // this.ship.rotation += 0.07;
+        this.ship.body.angularVelocity = 300;
+    } else {
+        this.ship.body.angularVelocity = 0;
+    }
+    allowShipReflow();
+}
+
+function allowShipReflow() {
+    if (this.ship.x < 0) {
+        this.ship.x = game.width;
+    }
+    else if (this.ship.x > game.width) {
+        this.ship.x = 0;
+    }
+
+    if (this.ship.y < 0) {
+        this.ship.y = game.height;
+    }
+    else if (this.ship.y > game.height) {
+        this.ship.y = 0;
+    }
 }
--- Asteroid/Scripts/game.js
+++ Asteroid/Scripts/game.js
@@ -11,6 +11,14 @@ var GameState = {
         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         this.scale.pageAlignHorizontally = true;
         this.scale.pageAlignVertically = true;
+
+        //  This will run in Canvas mode, so let's gain a little speed and display
+        game.renderer.clearBeforeRender = false;
+        game.renderer.roundPixels = true;
+
+
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
     },
 
     preload: function () {
