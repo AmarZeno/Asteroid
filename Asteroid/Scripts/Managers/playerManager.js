f134c8bae0787ad9957ca1f0891610853375f5b5
Zeno Saviourmarsalin <u1077160@ad.utah.edu>
2016-09-15 17:23:08 -0600

    Added left and right movements

--- Asteroid/Scripts/Managers/playerManager.js
+++ Asteroid/Scripts/Managers/playerManager.js
@@ -1,17 +1,34 @@
-﻿
+﻿var arrowKeys;
+
+// Extended system preload method
 function playerManagerLoad(thisGame) {
     thisGame.load.image('ship', 'Assets/Images/ship.png');
 }
 
+// Extended system draw method
 function playerManagerCreate(thisGame) {
-    createPlayer(thisGame);
+    addPlayer(thisGame);
+    addPlayerControls(thisGame);
 }
 
-function playerManagerUpdate() {
-
+// Extended system update method
+function playerManagerUpdate(thisGame) {
+    if (arrowKeys.left.isDown) {
+        this.ship.rotation += 0.02;
+    } else if (arrowKeys.right.isDown) {
+        this.ship.rotation -= 0.02;
+    }
 }
 
-function createPlayer(thisGame) {
-    this.ship = thisGame.add.sprite(10, 10, 'ship');
+
+// Custom Accessors
+
+function addPlayer(thisGame) {
+    this.ship = thisGame.add.sprite(this.game.world.centerX, this.game.world.centerY, 'ship');
     this.ship.scale.setTo(0.1);
+    this.ship.anchor.setTo(0.5);
+}
+
+function addPlayerControls(thisGame) {
+    arrowKeys = thisGame.game.input.keyboard.createCursorKeys();
 }
