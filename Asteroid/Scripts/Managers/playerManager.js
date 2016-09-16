9c440772508c888f29d27be13fa304130f1e279d
Zeno Saviourmarsalin <u1077160@ad.utah.edu>
2016-09-15 18:24:09 -0600

    Added laser logic

--- Asteroid/Scripts/Managers/playerManager.js
+++ Asteroid/Scripts/Managers/playerManager.js
@@ -1,4 +1,12 @@
-﻿var arrowKeys;
+﻿/*
+VARIABLES
+*/
+
+var arrowKeys;
+
+var laser;
+var laserCollection;
+var laserTime = 0;
 
 /*
 ENGINE CALLS
@@ -12,6 +20,7 @@ function playerManagerLoad(thisGame) {
 function playerManagerCreate(thisGame) {
     addPlayer(thisGame);
     addPlayerControls(thisGame);
+    createLaserCollection();
 }
 
 // Extended system update method
@@ -36,23 +45,27 @@ function addPlayerControls(thisGame) {
 }
 
 function capturePlayerActions() {
+    // Acceleration
     if (arrowKeys.up.isDown) {
         game.physics.arcade.accelerationFromRotation(this.ship.rotation, 200, this.ship.body.acceleration);
-    }
-    else {
+    } else {
         this.ship.body.acceleration.set(0);
     }
 
+    // Rotation
     if (arrowKeys.left.isDown) {
-        //this.ship.rotation -= 0.07;
         this.ship.body.angularVelocity = -300;
     } else if (arrowKeys.right.isDown) {
-        // this.ship.rotation += 0.07;
         this.ship.body.angularVelocity = 300;
     } else {
         this.ship.body.angularVelocity = 0;
     }
+
     allowShipReflow();
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+        fireLaser();
+    }
 }
 
 function allowShipReflow() {
@@ -68,5 +81,31 @@ function allowShipReflow() {
     }
     else if (this.ship.y > game.height) {
         this.ship.y = 0;
+    }
+}
+
+function createLaserCollection() {
+    //  Our ships lasers
+    laserCollection = game.add.group();
+    laserCollection.enableBody = true;
+    laserCollection.physicsBodyType = Phaser.Physics.ARCADE;
+
+    //  All 40 of them
+    laserCollection.createMultiple(2, 'laser');
+    laserCollection.setAll('anchor.x', 0.5);
+    laserCollection.setAll('anchor.y', 0.5);
+}
+
+function fireLaser() {
+    if (game.time.now > laserTime) {
+        laser = laserCollection.getFirstExists(false);
+
+        if (laser) {
+            laser.reset(this.ship.body.x + 16, this.ship.body.y + 16);
+            laser.lifespan = 2000;
+            laser.rotation = this.ship.rotation;
+            game.physics.arcade.velocityFromRotation(this.ship.rotation, 400, laser.body.velocity);
+            laserTime = game.time.now + 50;
+        }
     }
 }
