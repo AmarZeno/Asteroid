340d17e6af6b788a4e90d2fb4f8dcacc86f12818
Zeno Saviourmarsalin <u1077160@ad.utah.edu>
2016-09-20 11:00:09 -0600

    Added chelseys merge

---
+++ Asteroid/Scripts/Managers/playerManager.js
@@ -0,0 +1,135 @@
+﻿/*
+VARIABLES
+*/
+
+var arrowKeys;
+
+var laser;
+var laserCollection;
+var laserTime = 0;
+
+/*
+ENGINE CALLS
+*/
+// Extended system preload method
+function playerManagerLoad(thisGame) {
+    thisGame.load.image('ship', 'Assets/Images/ship.png');
+    thisGame.load.image('laser', 'Assets/Images/blue_laser.png');
+    //thisGame.OVERLAP_BIAS = 50;
+}
+
+// Extended system draw method
+function playerManagerCreate(thisGame) {
+    addPlayer(thisGame);
+    addPlayerControls(thisGame);
+    createLaserCollection();
+}
+
+// Extended system update method
+function playerManagerUpdate(thisGame) {
+    capturePlayerActions();
+    checkPlayerCollision();
+    checkLaserCollision();
+}
+
+
+/*
+CUSTOM ACCESSORS
+*/
+
+function addPlayer(thisGame) {
+    this.ship = thisGame.add.sprite(this.game.world.centerX, this.game.world.centerY, 'ship');
+    this.ship.scale.setTo(2);
+    this.ship.anchor.setTo(0.5);
+    // this.ship.anchor
+    game.physics.enable(this.ship, Phaser.Physics.ARCADE);
+}
+
+function addPlayerControls(thisGame) {
+    arrowKeys = thisGame.game.input.keyboard.createCursorKeys();
+}
+
+function capturePlayerActions() {
+    // Acceleration
+    if (arrowKeys.up.isDown) {
+        game.physics.arcade.accelerationFromRotation(this.ship.rotation, 200, this.ship.body.acceleration);
+    } else {
+        this.ship.body.acceleration.set(0);
+    }
+
+    // Rotation
+    if (arrowKeys.left.isDown) {
+        this.ship.body.angularVelocity = -300;
+    } else if (arrowKeys.right.isDown) {
+        this.ship.body.angularVelocity = 300;
+    } else {
+        this.ship.body.angularVelocity = 0;
+    }
+
+    screenWrap(this.ship, 0);
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+        fireLaser();
+    }
+}
+
+
+function createLaserCollection() {
+    //  Our ships lasers
+    laserCollection = game.add.group();
+    laserCollection.enableBody = true;
+    laserCollection.physicsBodyType = Phaser.Physics.ARCADE;
+
+    //  Create one set of shooting at at time
+    laserCollection.createMultiple(1, 'laser');
+    laserCollection.setAll('anchor.x', 0.5);
+    laserCollection.setAll('anchor.y', 0.5);
+    laserCollection.setAll('name', "laser");
+}
+
+function fireLaser() {
+    if (game.time.now > laserTime) {
+        laser = laserCollection.getFirstExists(false);
+
+        if (laser) {
+            laser.reset(this.ship.body.x + 16, this.ship.body.y + 16);
+            laser.lifespan = 2000;
+            laser.rotation = this.ship.rotation;
+            laser.scale.setTo(0.3);
+            game.physics.arcade.velocityFromRotation(this.ship.rotation, 500, laser.body.velocity);
+            laserTime = game.time.now + 500;
+        }
+    }
+}
+
+function checkPlayerCollision() {
+    game.physics.arcade.collide(this.ship, Asteroids_Red, playerRespawn, null, this);
+    game.physics.arcade.collide(this.ship, Asteroids_Grey, playerRespawn, null, this);
+    game.physics.arcade.collide(this.ship, Asteroids_Red_Med, playerRespawn, null, this);
+    game.physics.arcade.collide(this.ship, Asteroids_Grey_Med, playerRespawn, null, this);
+    game.physics.arcade.collide(this.ship, Asteroids_Red_Small, playerRespawn, null, this);
+    game.physics.arcade.collide(this.ship, Asteroids_Grey_Small, playerRespawn, null, this);
+}
+
+function checkLaserCollision() {
+    if (laser != null) {
+        game.debug.body(laser, 'red', false); game.debug.spriteBounds(this.laser, 'pink', false);
+    }
+    game.physics.arcade.overlap(this.laser, Asteroids_Red, AsteroidsCollide, null, this);
+    game.physics.arcade.overlap(this.laser, Asteroids_Grey, AsteroidsCollide, null, this);
+    game.physics.arcade.overlap(this.laser, Asteroids_Red_Med, AsteroidsCollide, null, this);
+    game.physics.arcade.overlap(this.laser, Asteroids_Grey_Med, AsteroidsCollide, null, this);
+    game.physics.arcade.overlap(this.laser, Asteroids_Red_Small, AsteroidsCollide, null, this);
+    game.physics.arcade.overlap(this.laser, Asteroids_Grey_Small, AsteroidsCollide, null, this);
+}
+
+function playerRespawn(sprite1, sprite2) {
+    AsteroidsCollide(sprite1, sprite2);
+    updateLivesUI();
+    this.ship.body.angularVelocity = 0;
+    this.ship.x = this.game.world.centerX;
+    this.ship.y = this.game.world.centerY;
+    this.ship.body.acceleration.set(0);
+    this.ship.body.velocity.setTo(0, 0);
+    this.ship.rotation = 0;
+}
---
+++ Asteroid/Scripts/game.js
@@ -0,0 +1,57 @@
+﻿/// <reference path="Phaser.js" />
+/// <reference path="Managers/configurationManager.js" />
+/// <reference path="Managers/playerManager.js" />
+
+var game = new Phaser.Game(1920, 1080, Phaser.AUTO);
+
+var GameState = {
+
+    // initialize game settings
+    init: function () {
+
+        configurationManagerInit(this);
+
+        // Start physics system
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    },
+
+    preload: function () {
+        // Load asset
+        LoadBackground(this);
+        playerManagerLoad(this);
+        AsteroidsLoad();
+        preloadUI(this);
+    },
+
+    create: function () {
+        // Access loaded asset
+       // this.background = this.add.sprite(0, 0, 'background');
+        drawBackground(this);
+        playerManagerCreate(this);
+        AsteroidsCreate();
+        initUI(this);
+    },
+
+    update: function () {
+        // Game loop
+        if (!gameOver) {
+            playerManagerUpdate(this);
+        }
+        AsteroidsUpdate();
+    }
+};
+
+game.state.add('GameState', GameState);
+game.state.start('GameState');
+
+/*
+CUSTOM ACCESSORS
+*/
+
+function LoadBackground(thisGame) {
+    thisGame.load.image('background', 'Assets/Images/background.jpg');
+}
+
+function drawBackground(thisGame) {
+    thisGame.background = thisGame.add.sprite(0, 0, 'background');
+}
